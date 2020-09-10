6b21999a7de382ff5cc07909e9935fb50ed00724
Filip Risteski <filip22risteski@gmail.com>
2020-09-10 21:46:19 +0200

    1.100920

---
+++ ElibForumsData/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ElibForumsData
+{
+    public class Class1
+    {
+    }
+}
---
+++ elib-custom-forum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace elib_custom_forum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
